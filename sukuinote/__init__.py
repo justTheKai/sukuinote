d69ebc2516f97e45cbd061c6584915c6ebcabc04
blank X <theblankx@protonmail.com>
2020-10-31 21:59:35 +0700

    should fix format_bytes or something

--- sukuinote/__init__.py
+++ sukuinote/__init__.py
@@ -129,7 +129,7 @@ def public_log_errors(func):
 def format_bytes(size):
     size = int(size)
     # 2**10 = 1024
-    power = 1000
+    power = 1024
     n = 0
     power_labels = {0 : '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
